cdc991e3325e2c2f99b43700f3a77b14beeb40a0
RajatChugh2711 <rajatchugh.work@gmail.com>
2025-08-26 00:41:20 +0530

    utils discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+// production based method for handler
+const asyncHandler = (func) => async (req, res, next) => {
+    try {
+        await Promise.resolve(func(req, res, next)).catch((err) => next(err))
+    } catch (error) {
+        res.status(error?.code || 500).json({ success: false, message: error?.message })
+    }
+}
+
+export { asyncHandler }
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+//         await func(req, res, next);
+//     } catch (error) {
+//         res.status(error?.code || 500).json({ success: false, message: error?.message })
+//     }
+// }
